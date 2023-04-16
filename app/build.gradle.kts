3a3a1c61822d4dbbbfbbe5008295e46fb268c0cb
soo <onemask@naver.com>
2023-04-16 23:47:39 +0900

    add comopse navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,18 +54,23 @@ dependencies {
   implementation ("androidx.core:core-ktx:1.8.0")
   implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
   implementation ("androidx.activity:activity-compose:1.5.1")
-  implementation ("androidx.compose.ui:ui")
-  implementation ("androidx.compose.ui:ui-graphics")
-  implementation ("androidx.compose.ui:ui-tooling-preview")
-  implementation ("androidx.compose.material3:material3")
+
   testImplementation ("unit:junit:4.13.2")
   androidTestImplementation ("androidx.test.ext:junit:1.1.3")
   androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
-  androidTestImplementation ("androidx.compose.ui:ui-test-junit4")
-  debugImplementation ("androidx.compose.ui:ui-tooling")
-  debugImplementation ("androidx.compose.ui:ui-test-manifest")
 
   val composeBom = platform("androidx.compose:compose-bom:2023.01.00")
   implementation (composeBom)
   androidTestImplementation (composeBom)
+  implementation ("androidx.compose.ui:ui")
+  androidTestImplementation ("androidx.compose.ui:ui-test-junit4")
+  debugImplementation ("androidx.compose.ui:ui-tooling")
+  implementation ("androidx.compose.ui:ui-graphics")
+  implementation ("androidx.compose.ui:ui-tooling-preview")
+  implementation ("androidx.compose.material3:material3")
+  debugImplementation ("androidx.compose.ui:ui-test-manifest")
+
+  val nav_version = "2.5.3"
+  implementation("androidx.navigation:navigation-compose:$nav_version")
+
 }

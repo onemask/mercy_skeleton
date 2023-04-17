5c2ed756f960041c75c87481ea6ad6ca606d6cd4
soo <onemask@naver.com>
2023-04-17 23:52:33 +0900

    add comopsable

--- app/src/main/java/com/mercy/skeleton/MainActivity.kt
+++ app/src/main/java/com/mercy/skeleton/MainActivity.kt
@@ -3,12 +3,16 @@ package com.mercy.skeleton
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.mercy.skeleton.ui.theme.MercySkeletonTheme
 
@@ -19,7 +23,7 @@ class MainActivity : ComponentActivity() {
       MercySkeletonTheme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Greeting("Android")
+          OnBoardingScreen("I'm a composable!")
         }
       }
     }
@@ -27,17 +31,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
+fun OnBoardingScreen(contents: String) {
+  Column(modifier = Modifier.fillMaxWidth()) {
+    Text(text = "OnBoarding", style = MaterialTheme.typography.headlineLarge)
+    Text(text = contents)
+
+    Image(
+      painter = painterResource(id = R.drawable.ic_launcher_background),
+      contentDescription = null
+    )
+  }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
   MercySkeletonTheme {
-    Greeting("Android")
+    OnBoardingScreen("Android")
   }
 }
